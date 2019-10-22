0efbae2e3d90e7a585c30597d05a713d2b520095
ingrid <msagrerom@inee.edu.mx>
2019-10-22 18:40:00 -0500

    hasta la capa de manejo de errores con middleware

--- index.js
+++ index.js
@@ -45,10 +45,16 @@ const app = express();
 const { config } = require('./config/index');
 const  moviesApi = require('./routes/movies.js');
 
+const {logErrors, errorHandler } = require('./utils/middleware/errorHandlers.js');
 //Middleware Body Parser
 app.use(express.json());
 
 moviesApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
+
+
 
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
